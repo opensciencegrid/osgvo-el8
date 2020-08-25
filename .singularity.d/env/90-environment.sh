b5363ef1bb76adda0fc85e25905173dc8fa39125
Mats Rynge <rynge@isi.edu>
2020-08-25 10:40:49 -0700

    Updated for the new Singularity import

--- .singularity.d/env/90-environment.sh
+++
@@ -1,9 +0,0 @@
-# Custom environment shell code should follow
-
-# Disabled - please use osgvo-el7-cuda10 if you want this functionality
-#if [ "x$LD_LIBRARY_PATH" = "x" ]; then
-#    export LD_LIBRARY_PATH="/host-libs"
-#else
-#    export LD_LIBRARY_PATH="/host-libs:$LD_LIBRARY_PATH"
-#fi
-
